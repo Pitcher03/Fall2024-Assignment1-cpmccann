4b7d4193c7b05d809ee135c2bad6bff895a4e2d9
Pitcher03 <157501478+Pitcher03@users.noreply.github.com>
2024-09-23 21:27:03 -0500

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+"use strict"
 
-// Write your JavaScript code.
+function show(elem_id, display_type) {
+    document.getElementById(elem_id).style.display = display_type;
+    console.log("showed " + elem_id);
+}
+
+function hide(elem_id) {
+    document.getElementById(elem_id).style.display = "none";
+    console.log("hid " + elem_id);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("page content loaded");
+
+    var current_season = 1;
+
+    document.getElementById("watch").onclick = () => {
+        document.getElementById("container-2").scrollIntoView({ behavior: "smooth" });
+    };
+
+    document.getElementById("trailer-button").onclick = () => {
+        document.getElementById("preview").scrollIntoView({ behavior: "smooth" });
+        hide("label");
+        document.getElementById("preview").style.backgroundImage = "none !important";
+        show("trailer", "block");
+
+    };
+
+    document.getElementById('seasons').addEventListener('change', function () {
+        var new_season = this.value;
+
+        hide("season-" + current_season);
+        show("season-" + new_season, "table");
+
+        current_season = new_season;
+
+        console.log(current_season + ' clicked');
+    });
+});
